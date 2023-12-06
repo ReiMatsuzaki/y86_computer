1b5d2aec7a2b459743853c3a166025af6c435e0a
Matsuzaki Rei <matsuzaki.rei@gmail.com>
2023-12-06 10:28:27 +0900

    memo

--- src/yis.rs
+++ src/yis.rs
@@ -2,7 +2,15 @@ use std::fmt;
 
 use csapp::utils::print_bytes;
 
-const MEM_SIZE: usize = 10000;
+// assign 4kB(=4096 byte) for each process
+// proc0: 0x0000 - 0x1FFF
+// proc1: 0x1000 - 0x2FFF
+// ...
+// procF: 0xF000 - 0xFFFF
+// store 16 processes at most
+// 16 * 4kB = 64kB = 16 ** 4
+// simulate 16 bit address space
+const MEM_SIZE: usize = 0x10000;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Y8R {
